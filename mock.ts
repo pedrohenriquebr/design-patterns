4b6082ff61c4889cf71cc11689c26a56a472d087
PedroHenriqueBragaDaSilva <pedrohenriquebraga735@gmail.com>
2022-02-15 23:12:01 -0300

    add code

---
+++ mock.ts
@@ -0,0 +1,20 @@
+import { Emojis } from "./enums";
+
+export const FAKE_NEWS  = [
+    `This morning the president did a speech wearing only underwear\n on United Nations Organization. ${Emojis.COUNTRY}`,
+    `The Russia intend to invade the USA using Ice Cream. ${Emojis.ALERT}`,
+    `Maybe the Bitcoin will destroy the World ${Emojis.MONEY}`,
+    `Why don't you buy a new car? ${Emojis.CAR}`,
+    `The duck quacks ${Emojis.QUACK} and my girlfriend farts ${Emojis.SQUEAK}`,
+    `'I hate my life' said the Bill Gates. Wow, surprise mother fucker, gimme your life so ${Emojis.SURPRISE}`,
+    `This Sunday it will be a sunny day. ${Emojis.SUNNY}`,
+    `The Last Of Us will be released in the end of the year. ${Emojis.SURPRISE}`,
+    `AI expert, Abdu Bilau Khaled asks if Bitcoin is the name of Brazilian food, and yes it is. ${Emojis.GOSSIPYNEIGHBOR}`,
+    `Maria will be a Queen. How it will be, I dunno ${Emojis.SQUEAK}`,
+    `The news is boring. I knew it, but it's my breadwinner ${Emojis.NOSOUND}`,
+    `Our director uses the word 'Bitcoin' in his speech. ${Emojis.QUACK}`,
+    `Bill Clinton used again his girlfriend panties to hide money. ${Emojis.HEHE}${Emojis.MONEY}${Emojis.WOW}`,
+    `Women\'s Bra Stealer was arrested yesterday at night using panties on his head ${Emojis.HEHE}`,
+    `'Quem é o cantor?!' said Tiririca ${Emojis.QUACK}`,
+    `Donald Trump is reptile, affirms a Harvard professor. ${Emojis.WTF}`,
+  ];
